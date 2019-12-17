b826eebefbc7be1cd1cbfd028883716908a8bbac
Zaky98 <zakyluthfansa@gmail.com>
2019-12-17 23:39:43 +0700

    listview with simple adapter

--- app/src/main/java/com/example/projectpresetlightroom/activities/MainActivity.java
+++ app/src/main/java/com/example/projectpresetlightroom/activities/MainActivity.java
@@ -4,7 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.app.PendingIntent;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.example.projectpresetlightroom.R;
@@ -12,15 +19,6 @@ import com.example.projectpresetlightroom.fragements.More;
 import com.example.projectpresetlightroom.fragements.MyCollection;
 import com.example.projectpresetlightroom.fragements.Presets;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-//public class MainActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//    }
-//}
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -30,14 +28,17 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         setContentView(R.layout.activity_main);
         loadFragment(new Presets());
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
-        // beri listener pada saat item/menu bottomnavigation terpilih
+        // beri listener pada saat item/menu bottomnavigation terpili
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
     }
 
-//    private void loadFragment(FoodFragment foodFragment) {
-//    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-    private boolean loadFragment(Fragment fragment) {
+        private boolean loadFragment(Fragment fragment) {
         if (fragment != null) {
             getSupportFragmentManager()
                     .beginTransaction()
@@ -59,9 +60,6 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
             case R.id.action_collection:
                 fragment = new MyCollection();
                 break;
-            case R.id.action_more:
-                fragment = new More();
-                break;
         }
 
         return loadFragment(fragment);
@@ -72,5 +70,32 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
     }
 
+    public void email(MenuItem item) {
+        try{
+            Intent intent = new Intent (Intent.ACTION_VIEW , Uri.parse("mailto:" + "zakyluthfansa@gmail.com"));
+            intent.putExtra(Intent.EXTRA_TEXT, "Hello, thanks for using our app. How we can help?");
+            startActivity(intent);
+        }catch(ActivityNotFoundException e){
+            //TODO smth
+        }
+    }
+
+    public void instagram(MenuItem item) {
+        Uri uri = Uri.parse("https://www.instagram.com/luthfansa_");
+        Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+        likeIng.setPackage("com.instagram.android");
+
+        try {
+            startActivity(likeIng);
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.instagram.com/luthfansa_/")));
+        }
+    }
+
+    public void works(MenuItem item) {
+        startActivity(new Intent(this, HowItWorks.class).putExtra("from" , "previousActivity"));
+    }
 }
 
--- app/src/main/java/com/example/projectpresetlightroom/fragements/Presets.java
+++ app/src/main/java/com/example/projectpresetlightroom/fragements/Presets.java
@@ -1,29 +1,105 @@
 package com.example.projectpresetlightroom.fragements;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.example.projectpresetlightroom.R;
+import com.example.projectpresetlightroom.activities.Detail;
+//import com.example.projectpresetlightroom.adapters.PresetAdapters;
+//import com.example.projectpresetlightroom.models.PresetModels;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Presets extends Fragment {
+
+//    RecyclerView rvPreset;
+//    List<PresetModels> listPreset = new ArrayList<>();
+
+    protected ListView lv;
+    protected ListAdapter adapter;
+    SimpleAdapter Adapter;
+    HashMap<String, String> map;
+    ArrayList<HashMap<String, String>> mylist;
+    String[] Pil;
+    String[] Ltn;
+    String[] Gbr;
 
     public Presets() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_presets, container, false);
+//        return inflater.inflate(R.layout.fragment_presets, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_presets, container, false);
+
+//        //menyambungkan rvSuperHero ke layout
+//        rvPreset = rootView.findViewById(R.id.rvSuperHero);
+//        PresetModels hero = new PresetModels("Petruk", Integer.toString(R.drawable.brush));
+//        //menambahkan hero ke listSuperHero
+//        listPreset.add(hero);
+//        //membuat object hero baru
+//        hero = new PresetModels("Gareng", Integer.toString(R.drawable.brush));
+//        //menambahkan hero baru ke listSuperhero
+//        listPreset.add(hero);
+//        //Instansiasi Adapter
+//        PresetAdapters superHeroAdapter = new PresetAdapters(listPreset);
+//        //set adapter dan layoutmanager
+//        rvPreset.setAdapter(superHeroAdapter);
+//        rvPreset.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+
+        lv = (ListView) rootView.findViewById(R.id.lv);
+
+        Pil = new String[] {"Summer", "Selfie", "Fall", "Jungle", "Sunset"};
+        Gbr = new String[] {Integer.toString(R.drawable.summer),
+                Integer.toString(R.drawable.selfie),
+                Integer.toString(R.drawable.fall),
+                Integer.toString(R.drawable.jungle),
+                Integer.toString(R.drawable.sunset) };
+
+        mylist = new ArrayList<HashMap<String,String>>();
+
+        for (int i = 0; i < Pil.length; i++){
+            map = new HashMap<String, String>();
+            map.put("list", Pil[i]);
+            map.put("gbr", Gbr[i]);
+            mylist.add(map);
+        }
+
+        Adapter = new SimpleAdapter(getContext(), mylist, R.layout.list_preset,
+                new String[] {"list", "gbr"}, new int[] {R.id.tv_nama, R.id.imV});
+        lv.setAdapter(Adapter);
+
+        View imageview = rootView.findViewById(R.id.imV);
+        View textview  = rootView.findViewById(R.id.tv_nama);
+
+        imageview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), Detail.class);
+                if (Integer.toString(R.id.tv_nama) == "summer") {
+                    i.putExtra("title", "The Fate of the Furious (2017)");
+                    i.putExtra("desc", "Luke Hobbs (Dwayne Johnson) membentuk aliansi yang tidak mungkin " +
+                            "dengan Deckard Shaw (Jason Statham). Keduanya terpaksa bersatu membantu Hattie Shaw " +
+                            "(Vanessa Kirby) untuk memburu Brixton (Idris Elba). Brixton adalah penjahat jenis baru " +
+                            "yang berhasil mengubah dirinya menjadi " +
+                            "manusia super. Keberadaanya menjadi ancaman bagi umat manusia.");
+                    startActivity(i);
+                }
+            }
+        });
+
+        return rootView;
     }
 
 }
